9790ab1c77fb3a26b7d222950466c5695b601d3f
mavidalr <marcela.vidalr@alumnos.usm.cl>
2016-05-12 22:32:28 -0300

    Implementación Mostrar receta, sin datos

--- app/src/main/java/com/example/mache/recetario2/MostrarRecetaActivity.java
+++ app/src/main/java/com/example/mache/recetario2/MostrarRecetaActivity.java
@@ -1,31 +1,88 @@
 package com.example.mache.recetario2;
 
-import android.os.Bundle;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ImageView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.example.mache.recetario2.app.AppController;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MostrarRecetaActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_receta);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolBarLayout.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.MyToolbar);
+        //setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        collapsingToolbar.setTitle("My Toolbar Tittle");
+
+        //para agregar el fondo
+        ImageView targetImage = (ImageView)findViewById(R.id.ImagenFondo);
+
+        //Load bitmap from internet
+        String onLineImage = "https://www.meals.com/ImagesRecipes/146232lrg.jpg";
+        URL onLineURL;
+
+        try {
+            onLineURL = new URL(onLineImage);
+            new MyNetworkTask(targetImage).execute(onLineURL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private class MyNetworkTask extends AsyncTask<URL, Void, Bitmap>{
+
+        ImageView tIV;
+
+        public MyNetworkTask(ImageView iv){
+            tIV = iv;
+        }
+
+        @Override
+        protected Bitmap doInBackground(URL... urls) {
+
+            Bitmap networkBitmap = null;
+
+            URL networkUrl = urls[0]; //Load the first element
+            try {
+                networkBitmap = BitmapFactory.decodeStream(
+                        networkUrl.openConnection().getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+
+            return networkBitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            tIV.setImageBitmap(result);
+        }
+
     }
 }
+
+
