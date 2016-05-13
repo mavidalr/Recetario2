c864214348ba5429a5f280e4bc2b4132c32fe882
mavidalr <marcela.vidalr@alumnos.usm.cl>
2016-05-12 21:29:43 -0300

    ...

--- app/src/main/java/com/example/mache/recetario2/MostrarRecetaActivity.java
+++ app/src/main/java/com/example/mache/recetario2/MostrarRecetaActivity.java
@@ -1,6 +1,7 @@
 package com.example.mache.recetario2;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,8 @@ public class MostrarRecetaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mostrar_receta);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +28,4 @@ public class MostrarRecetaActivity extends AppCompatActivity {
             }
         });
     }
-
 }
